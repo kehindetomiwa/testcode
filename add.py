63af0ddce76bb58a5a0b1b8e5df76e0a4623dc5c
kehindetomiwa <kehinde.tomiwa4@gmail.com>
2023-06-12 21:49:03 +0200

    test commit

---
+++ add.py
@@ -0,0 +1,17 @@
+
+
+def addition(a, b):
+    f_a_b = a + b
+    return f_a_b
+
+def substraction(a, b):
+    f_a_b = a - b
+    return f_a_b
+
+
+ans = addition(1000, 40000)
+print(ans)
+
+ans2 = substraction(4000, 8)
+print(ans2)
+
