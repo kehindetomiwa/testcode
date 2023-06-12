cbde6e0b991af71a4e7a2c1ccee345a656499424
kehindetomiwa <kehinde.tomiwa4@gmail.com>
2023-06-12 21:57:46 +0200

    added 3 para calculation

---
+++ add2 copy.py
@@ -0,0 +1,17 @@
+
+
+def addition(a, b, c):
+    f_a_b = a + b + c
+    return f_a_b
+
+def substraction(a, b, c):
+    f_a_b = a - b - c
+    return f_a_b
+
+
+ans = addition(1000, 40000, 2000)
+print(ans)
+
+ans2 = substraction(4000, 8, 100)
+print(ans2)
+
